9c26611faf62b3433934656f92dc12c08779e971
danvar249 <danvar249@gmail.com>
2023-07-13 19:18:53 +0300

    Can add courses to user

--- src/components/ManageCourses.tsx
+++ src/components/ManageCourses.tsx
@@ -10,6 +10,8 @@ import { User, Course, UserType } from '../data/types';
 const ManageCourses = () => {
     const [users, setUsers] = useState<User[]>([]);
     const [selectedUser, setSelectedUser] = useState<User | null>(null);
+    const [coursesForUser, setCoursesForUser] = useState<Course[]>([]);
+    const [selectedCourse, setSelectedCourse] = useState<Course>();
     const [courses, setCourses] = useState<Course[]>([]);
     const [currentPage, setCurrentPage] = useState<number>(1);
     const [coursesPerPage, setCoursesPerPage] = useState<number>(10);
@@ -18,25 +20,41 @@ const ManageCourses = () => {
 
     useEffect(() => {
         managerDataOperations.getUsers(undefined).then(fetchedTeachers => setUsers(fetchedTeachers))
-
-
+        managerDataOperations.getCourses().then(fetchedCourses => setCourses(fetchedCourses));
     }, []);
 
     useEffect(() => {
         if (!selectedUser)
             return;
-        managerDataOperations.getCoursesOfTeacher(selectedUser.id).then(fetchedCourses => setCourses(fetchedCourses))
-
+        managerDataOperations.getCoursesOfTeacher(selectedUser.id).then(fetchedCourses => setCoursesForUser(fetchedCourses))
     }, [selectedUser]);
+
+    useEffect(() => {
+        const filtered = courses.filter((c: Course) => {
+            if (coursesForUser.find(c1 => c1.id === c.id))
+                return false;
+            return true;
+        });
+        setCourses(filtered);
+    }, [coursesForUser]);
 
     const handleUserChange = (selectedOption: User | null) => {
         setSelectedUser(selectedOption);
     };
-
+    const handleAddCourse = async () => {
+        if (!selectedUser || !selectedCourse)
+            return;
+        try {
+            await managerDataOperations.addCourseForTeacher(selectedUser?.id, selectedCourse?.id);
+        } catch (error) {
+            console.error(`Error when adding course: ${error}`);
+        }
+        alert(`${selectedCourse.name} successfully added to ${selectedUser.name}!`);
+        managerDataOperations.getCourses().then(fetchedCourses => setCourses(fetchedCourses));
+    }
     const handleRemoveCourse = async () => {
         console.log('Remove Course:', courseToRemove);
 
-        setCourses(courses.filter((course) => course.id !== courseToRemove.id));
         setShowDeleteModal(false);
 
         try {
@@ -44,6 +62,7 @@ const ManageCourses = () => {
         } catch (error) {
             console.error(`Error when removing course: ${error}`);
         }
+        managerDataOperations.getCourses().then(fetchedCourses => setCourses(fetchedCourses));
     };
 
     const handleDeleteConfirmation = (course: Course) => {
@@ -65,64 +84,79 @@ const ManageCourses = () => {
             </div>
         );
     }
+
+
     return (
         <><Container fluid>
             <h1 className="mb-4 text-center">Manage Courses</h1>
             <hr className="section-separator" />
 
-            <div className="col-md-5 mb-3">
-                <Row>
-                    <Col>
+            <Row className='form-row'>
+                <Col sm={5} className='form-item'>
                         <Select
                             options={users}
                             value={selectedUser}
                             onChange={handleUserChange}
                             getOptionLabel={(option) => option.name}
                             getOptionValue={(option) => option.id.toString()}
-                            placeholder="Select Teacher or Student"
-                            isClearable />
+                        placeholder="Select Teacher or Student..."
+                        isClearable />
+                </Col>
+                {selectedUser && (
+                    <><Col sm={5} className='form-item'>
+                        <Select
+                            options={courses}
+                            value={selectedCourse}
+                            onChange={(course) => setSelectedCourse(course || undefined)}
+                            getOptionLabel={(option) => option.name}
+                            getOptionValue={(option) => option.id.toString()}
+                            placeholder="Select Course..." />
                     </Col>
-                </Row>
-            </div>
+                        <Col className='form-item'>
+                            <Button onClick={handleAddCourse}>Add</Button>
+                        </Col></>
+                )}
+            </Row>
 
-            <div className="table-wrapper">
-                <Table striped bordered hover>
-                    <thead>
-                        <tr>
-                            <th>Course Code</th>
-                            <th>Course Name</th>
-                            <th>Actions</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {courses
-                            .slice((currentPage - 1) * coursesPerPage, currentPage * coursesPerPage)
-                            .map((course, index) => (
-                                <tr key={index}>
-                                    <td>{course.id}</td>
-                                    <td>{course.name}</td>
-                                    <td>
-                                        <Button
-                                            variant="danger"
-                                            onClick={() => handleDeleteConfirmation(course)}
-                                        >
-                                            Delete
-                                        </Button>
-                                    </td>
-                                </tr>
-                            ))}
-                    </tbody>
-                </Table>
-            </div>
-
-            <PaginationComponent
-                currentPage={currentPage}
-                itemsPerPage={coursesPerPage}
-                totalItems={courses.length}
-                onPageChange={setCurrentPage}
-                onItemsPerPageChange={(itemsPerPage) => setCoursesPerPage(itemsPerPage)}
-                availableItemsPerPage={[10, 20, 50]} />
-        </Container><ConfirmModal
+            {selectedUser && (
+                <><div className="table-wrapper">
+                    <Table striped bordered hover>
+                        <thead>
+                            <tr>
+                                <th>Course Code</th>
+                                <th>Course Name</th>
+                                <th>Actions</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {coursesForUser
+                                .slice((currentPage - 1) * coursesPerPage, currentPage * coursesPerPage)
+                                .map((course, index) => (
+                                    <tr key={index}>
+                                        <td>{course.id}</td>
+                                        <td>{course.name}</td>
+                                        <td>
+                                            <Button
+                                                variant="danger"
+                                                onClick={() => handleDeleteConfirmation(course)}
+                                            >
+                                                Delete
+                                            </Button>
+                                        </td>
+                                    </tr>
+                                ))}
+                        </tbody>
+                    </Table>
+                </div><PaginationComponent
+                        currentPage={currentPage}
+                        itemsPerPage={coursesPerPage}
+                        totalItems={coursesForUser.length}
+                        onPageChange={setCurrentPage}
+                        onItemsPerPageChange={(itemsPerPage) => setCoursesPerPage(itemsPerPage)}
+                        availableItemsPerPage={[10, 20, 50]} /></>
+            )}
+        </Container>
+            <ConfirmModal
                 show={showDeleteModal}
                 onHide={() => setShowDeleteModal(false)}
                 onConfirm={handleRemoveCourse}
